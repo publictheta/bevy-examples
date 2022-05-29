bdfa111ef86bcd7be27a3c7d68eca3a497f58cbc
Public Theta <publictheta@gmail.com>
2022-05-29 19:42:42 +0900

    update examplesをcratesに名前変更

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,24 @@ set -eu
 
 function deploy {
     local PACKAGE_NAME="$1"
-    cd "$(dirname "$BASH_SOURCE")/examples/$PACKAGE_NAME"
+
+    local PACKAGE_DIR="$(dirname "$BASH_SOURCE")/crates/$PACKAGE_NAME"
+
+    if [ ! -d "$PACKAGE_DIR" ]; then
+        echo "ERROR: $PACKAGE_DIR is not a directory." >&2
+        exit 1
+    fi
+
+    local PACKAGE_HTML="$PACKAGE_DIR/index.html"
+
+    if [ ! -f "$PACKAGE_HTML" ]; then
+        echo "ERROR: $PACKAGE_HTML is not a file." >&2
+        exit 1
+    fi
+
+    trunk build -- "$PACKAGE_HTML"
+
+    cd "$PACKAGE_DIR"
     netlify deploy --dir=dist --prod
 }
 
